d581e83b969e5034cad0e789f7b98403403678a1
Valeriia Kolesnyk <kolesnik.va8@gmail.com>
2023-04-02 01:03:04 +0100

    Completed third task in Lesson 1
    Added ucFirst function to automatically capitalize the first letter of a string
    No need to do it manually anymore :)

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,14 @@
 function ucFirst(str) {
-  // ваш код...
+  // Сheck if the string is empty
+  if (!str) {
+    return str;
+  }
+  // Get the first character of the string and convert it to uppercase
+  const firstChar = str[0].toUpperCase();
+
+  // Get the rest of the characters in the string
+  const lastChars = str.slice(1);
+
+  //Combine the uppercase first character with the rest of the characters and return the result
+  return `${firstChar}${lastChars}`;
 }
