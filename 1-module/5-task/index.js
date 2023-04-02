88bf988c43c990d9c42ffcd5efcdd34c74d8c84d
Valeriia Kolesnyk <kolesnik.va8@gmail.com>
2023-04-02 03:47:01 +0100

    Completed fifth task in Lesson 1
    Added ellipsis to strings that exceed maxlength when truncated, and otherwise returns the original string

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,9 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  if (str && str.length > maxlength) {
+    return str.slice(0, maxlength - 1) + "…";
+    /* The -1 in `maxlength - 1` accounts for the added ellipsis,
+    preventing the final length from exceeding maxlength */
+  }
+  return str;
 }
+
