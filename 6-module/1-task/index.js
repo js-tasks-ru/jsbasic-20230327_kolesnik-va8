7c65548ffc440e184a16290d52a371138b5c6722
Valeriia Kolesnyk <kolesnik.va8@gmail.com>
2023-04-24 18:42:38 +0100

    Completed first task in Lesson 6
    (6-1) Add UserTable class with methods to create and modify a table

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,52 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this._rows = [];
+    this.elem = this._createTable();
+    this.rows = rows;
+  }
+
+  get rows() {
+    return this._rows;
+  }
+
+  set rows(rows) {
+    this._rows = rows;
+    let tableBody = this.elem.querySelector('tbody');
+    tableBody.innerHTML = '';
+    this.rows.forEach((row) => {
+      let tr = document.createElement('tr');
+      Object.values(row).forEach((value) => {
+        let td = document.createElement('td');
+        td.textContent = value;
+        tr.appendChild(td);
+      });
+      let deleteButton = document.createElement('button');
+      deleteButton.textContent = 'X';
+      deleteButton.addEventListener('click', () => {
+        tableBody.removeChild(tr);
+      });
+      let td = document.createElement('td');
+      td.appendChild(deleteButton);
+      tr.appendChild(td);
+      tableBody.appendChild(tr);
+    });
+  }
+
+  _createTable() {
+    let table = document.createElement('table');
+    table.classList.add('user-table');
+
+    let headerRow = table.insertRow();
+    for (let key in this.rows[0]) {
+      let th = document.createElement('th');
+      th.textContent = key[0].toUpperCase() + key.slice(1);
+      headerRow.appendChild(th);
+    }
+
+    let tableBody = document.createElement('tbody');
+    table.appendChild(tableBody);
+
+    return table;
   }
 }
